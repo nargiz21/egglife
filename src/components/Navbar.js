5713625d883a09589b318bf3822b77beed2fa571
nargiz21 <ahmdva.nargiz@gmail.com>
2023-07-10 23:55:33 +0400

    introduction section created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,22 @@ import logo from "../images/logo.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import {colors} from "../data";
 
 const Navbar = () => {
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((v) => {
+        return v === 4 ? 0 : v + 1;
+      });
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="navbar-wrapper">
+    <div className="navbar-wrapper" style={{ backgroundColor: colors[value]}}>
       <div className="navbar-list">
         <div className="shop-list list">
           <a href="#">
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const headers = [
+  "low carb",
+  "minding macros",
+  "extra protein",
+  "gluten free",
+  "eating better",
+];
+export const colors = ["#FED141", "#ED8B00", "#93C90F", "#FDF38A", "#007aff"];
+export const images = ["orange", "red", "green", "yellow", "blue"];
